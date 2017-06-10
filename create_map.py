ca249cbf7e876515ed7846fb1d18cc6914d44d8c
J Dasenbrock <dasenbrockjw@gmail.com>
2017-06-10 07:36:59 -0500

    functional create_maps, used lookup function instead of df

--- create_map.py
+++ create_map.py
@@ -5,53 +5,31 @@ from mpl_toolkits.basemap import Basemap
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 from matplotlib.colors import Normalize
+import helpers
+import zipcode_tools
 
-
-# In[2]:
-
-data = pk.unpickle_object('clean_data')
-data = data[data['total population'] < 50000]
-data = data[data['total population'] > 5000]
-
-
-# In[3]:
+lats, lons = helpers.get_lat_lon('zipcodes.csv')
 
 westlimit=-127.53; southlimit=24.17; eastlimit=-63.19; northlimit=50.32
 mid_lat = abs(westlimit - eastlimit)*0.5
 mid_lon = abs(northlimit - southlimit)*0.5
-
-
-# In[5]:
 
 m = Basemap(resolution='c', # c, l, i, h, f or None
             projection='merc',
             lat_0=mid_lat, lon_0=mid_lon,
             llcrnrlon=westlimit, llcrnrlat=southlimit, urcrnrlon=eastlimit, urcrnrlat=northlimit)
 
-
-# In[6]:
-
 m.drawmapboundary(fill_color='#46bcec')
 m.fillcontinents(color='#f2f2f2',lake_color='#46bcec')
 m.drawcoastlines()
 m.readshapefile('statesp020', 'statesp020')
-x,y = m(list(data['lons']), list(data['lats']))
+x,y = m(lons, lats)
 m.plot(x,y,'.', markersize='8')
 fig_size = plt.rcParams["figure.figsize"]
 fig_size[0] = fig_size[0]*4
 fig_size[1] = fig_size[1]*4
 plt.rcParams["figure.figsize"] = fig_size
-print(plt.rcParams["figure.figsize"])
 
 plt.savefig('filename.png', bbox_inches=0)
-
-
-# In[ ]:
-
-data.describe()
-
-
-# In[ ]:
-
 
 
--- helpers.py
+++ helpers.py
@@ -11,11 +11,24 @@ def read_csv(filename):
     return output
 
 
+def to_csv(alist):
+    with open('errors.csv', 'w+') as f:
+        for line in alist:
+            f.write('%s,\n' % line)
+
+
 def get_lat_lon(filename):
     """Returns two lists for lats and lons"""
     zipcodes = read_csv(filename)
-    lats = list(); lons = list()
+    lats = list(); lons = list(); errors = list();
     for zipcode in zipcodes:
         data = zipcode_tools.get_city_data_geolocator(zipcode) 
-        lats.append(data[0]); lons.append(data[1])
+        lat = data[0]; lon = data[1];
+        if isinstance(lat, float) and isinstance(lon, float):
+            lats.append(lat); lons.append(lon)
+        else:
+            errors.append(zipcode)
+    if len(errors) != 0:
+        to_csv(errors)
+        print('Errors written to file. Length: %s' % len(errors))
     return lats, lons
