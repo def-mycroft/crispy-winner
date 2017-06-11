ee40c5662c8faede7a9fc6cfae0c1745b9b5adf6
J Dasenbrock <dasenbrockjw@gmail.com>
2017-06-11 09:31:41 -0500

    zipcode distance finder can take None inputs

--- map_app/zipcode_tools.py
+++ map_app/zipcode_tools.py
@@ -39,4 +39,7 @@ def dist_between_zips(zipone, ziptwo):
     """Returns great-circle distance between two zip code"""
     zipone = get_city_data_geolocator(zipone)
     ziptwo = get_city_data_geolocator(ziptwo)
-    return great_circle(tuple(zipone[:2]), tuple(ziptwo[:2])).miles
+    if zipone != None and ziptwo != None:
+        return great_circle(tuple(zipone[:2]), tuple(ziptwo[:2])).miles
+    else:
+        return 0
