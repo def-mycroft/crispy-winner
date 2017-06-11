800e79776a161a73b1d62cc4b104b9a8eddbabc2
J Dasenbrock <dasenbrockjw@gmail.com>
2017-06-10 19:34:03 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='map_app',
+      version='0.1',
+      description='Givven a set of zipcodes, plot points on a map representing them',
+      url='https://github.com/def-mycroft/crispy-winner',
+      author='Joseph Dasenbrock',
+      author_email='dasenbrockjw@gmail.com',
+      license='MIT',
+      packages=['map_app'],
+      zip_safe=False)
